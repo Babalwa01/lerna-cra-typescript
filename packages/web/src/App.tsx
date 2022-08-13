012264136c5f6c1dca9a80798e9d63081932238b
Babalwa Mbolekwa <babalwa.mbolekwa@clearscore.com>
2022-08-13 23:23:12 +0200

    mono repo using lerna cra and typescript

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@babalwa/common";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button textColor="red">Hello, click me!</Button>
+    </div>
+  );
+}
+
+export default App;
